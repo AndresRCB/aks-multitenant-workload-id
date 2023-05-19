25b671e9bfdaa15afb02022d50eeff601b2ab749
Shawn Cicoria <github@cicoria.com>
2023-05-19 17:22:32 -0400

    adding delete of APP

--- apps/outputs.tf
+++ apps/outputs.tf
@@ -19,3 +19,8 @@ output "print_key_vault_secret_command2" {
   value = "kubectl exec -i deploy/${local.deployment_name} -n ${kubernetes_namespace.main.id} -- cat /mnt/secrets-store2/${var.secret_name2}"
   description = "Command to print the secondary key vault secret mounted in the kubernetes client deployment (a test)"
 }
+
+output "aad_application_id" {
+    value = azuread_application.main.application_id
+    description = "Application id that was registered for the service principal"
+}
